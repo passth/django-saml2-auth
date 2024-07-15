6f44d4ccd7c061c113dd9c21621cdeb192810ea8
Tiago Lira <tiago@passthrough.com>
2024-07-15 12:04:47 -0300

    Saml login

--- django_saml2_auth/urls.py
+++ django_saml2_auth/urls.py
@@ -5,6 +5,7 @@ app_name = 'django_saml2_auth'
 
 urlpatterns = [
     path("<uuid:metadata_id>/acs/", views.acs, name="acs"),
+    path("otp_login/", views.otp_login, name="otp_login"),
     re_path(r'^welcome/$', views.welcome, name="welcome"),
     re_path(r'^denied/$', views.denied, name="denied"),
 ]
